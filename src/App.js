656d3001ebf8f9fd4346fdbfd2c7008d24071c13
PrajuMaharjan <prajumaharjan903@gmail.com>
2026-03-06 22:12:23 +0545

    Initialized all necessary pages with dummy text and added them to App navigation. Note: this is only for Word Game portion

--- src/App.js
+++ src/App.js
@@ -3,18 +3,34 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 /* Import each screen*/
 import HomeScreen from './HomeScreen';
-import Settings from './Settings';
-import GameSettings from './GameSettings';
+import Settings from './SettingsScreen';
+import GameSettings from './GameSettingsScreen';
+
+/* Word Game Screens*/
+import Discussion from './WordGame/DiscussionScreen';
+import GenreSelect from './WordGame/GenreSelectScreen';
+import PlayerEntry from './WordGame/PlayerNamesScreen';
+import RoleReveal from './WordGame/RoleRevealScreen';
+import Voting from './WordGame/VotingScreen';
+import Results from './WordGame/ResultsScreen';
 
 const Stack=createNativeStackNavigator();
 
 export default function App() {
 return(
   <NavigationContainer>
-  <Stack.Navigator initialRouteName="Home">
+  <Stack.Navigator initialRouteName="Home"> 
   <Stack.Screen name='Home' component={HomeScreen} options={{headerShown:false}}/>
   <Stack.Screen name='GameSettings' component={GameSettings} options={{headerTitle:''}}/>
   <Stack.Screen name='Settings' component={Settings} />
+
+  <Stack.Screen name='Discussion' component={Discussion} />
+  <Stack.Screen name='Select Genre' component={GenreSelect} />
+  <Stack.Screen name='Names' component={PlayerEntry} />
+  <Stack.Screen name='Roles' component={RoleReveal} />
+  <Stack.Screen name='Vote' component={Voting} />
+  <Stack.Screen name='Results' component={Results} />
+
   </Stack.Navigator>
   </NavigationContainer>
 );
--- src/WordGame/PlayerNamesScreen.js
+++ src/WordGame/PlayerNamesScreen.js
@@ -1,11 +1,9 @@
 import {View,Text,Button,StyleSheet} from 'react-native';
 
-export default function GameSettings({navigation}){
+export default function EnterNames({navigation}){
     return(
         <View style={styles.container}>
-            <Text style={styles.title}>Game Settings</Text>
-
-            {/* <Button title="Back to Home" onPress={()=>navigation.navigate('Home')}/> */}
+            <Text style={styles.title}>Names</Text>
         </View>
     );
 }
@@ -14,12 +12,14 @@ const styles=StyleSheet.create({
     container:{
         flex:1,
         justifyContent:'center',
-        alighItems:'center',
+        alignItems:'center',
         backgroundColor:'white',
     },
     title:{
         fontSize:24,
         fontWeight:'bold',
         color:'black',
+        justifyContent:'center',
+        alignItems:'center',
     },
 });
