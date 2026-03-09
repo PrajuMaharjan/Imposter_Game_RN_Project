67631120350833a9c8403b58263d70cb336212ce
PrajuMaharjan <prajumaharjan903@gmail.com>
2026-03-09 21:18:47 +0545

    Made functions for naming logic in PlayerNamesScreen

--- src/WordGame/PlayerNamesScreen.js
+++ src/WordGame/PlayerNamesScreen.js
@@ -1,7 +1,51 @@
-import {View,Text,Button,StyleSheet} from 'react-native';
+import {View,Text,Button,StyleSheet,ImageBackground,TouchableOpacity,ScrollView,TextInput,Alert} from 'react-native';
+import {useState} from 'react';
+import {useGame} from '../GameContext';
 
 export default function EnterNames({navigation}){
+    const {gameState,setGameState}=useGame();
+    const initialPlayers=Array.from({length:gameState.players},(_,i)=>({
+        id:i+1,
+        name:`Player ${i+1}`,
+    }));
+
+    const [players,setPlayers]=useState(initialPlayers);
+    const [imposters,setImposters](gameState.imposters);
+    const [editingId,seteditingId]=useState(null);
+
+    const addPlayer=()=>{
+        const newId=players.length>0?Math.max(...players.map(p=>p.id))+1:1;
+        setPlayers(prev=>[...prev,{id:newId,name:`Player ${newId}`}]);
+    };
+
+    const removePlayer=(id)=>{
+        if(players.length<=3){
+            Alert.alert('Minimum players reached.','You need atleast 2 players');
+            return;
+        }
+        setPlayers(prev=>prev.filter(p=>p.id!==id));
+    };
+
+    const editName=(id,name)=>{
+        setPlayers(prev=>prev.map(p=>p.id===id?{...p,name}:p));
+    };
+
+    const handlePlay=()=>{
+        if(imposters>=players.length){
+            Alert.alert('Too many imposters','Must be less than number of total players.')
+            return;
+        }
+        setGameState(prev=>({
+            ...prev,
+            players:players.length,
+            imposters:imposters,
+            playerNames:players.map(p=>p.name),
+        }));
+        navigation.navigate('Roles');
+    };
+
     return(
+
         <View style={styles.container}>
             <Text style={styles.title}>Names</Text>
         </View>
