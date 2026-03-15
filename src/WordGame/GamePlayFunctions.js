fc012e42cd880b82cdfa8174e0cb16a74ffe2767
PrajuMaharjan <prajumaharjan903@gmail.com>
2026-03-15 11:45:31 +0545

    Converted Worddata.csv to WordData.json. Changed the shake functionality to use expo-haptics instead of react-native-shake.

--- src/WordGame/GamePlayFunctions.js
+++ src/WordGame/GamePlayFunctions.js
@@ -1,24 +1,11 @@
-import {Asset} from 'expo-asset';
-import * as FileSystem from 'expo-file-system';
-import Papa from 'papaparse';
+import wordData from '../../assets/Datasets/WordData.json';
 
 let _wordData = null;
 
 export async function loadWordData(){
     if (_wordData) return _wordData;
 
-    const asset=Asset.fromModule(require('../../assets/Datasets/WordData.csv'));
-    await asset.downloadAsync();
-
-    const csvString=await FileSystem.readAsStringAsync(asset.loadUri);
-    const result=Papa.parse(csvString,{header:true,skipEmptyLines:true,});
-
-    _wordData=result.data.map(row=>({
-        id:Number(row.id),
-        word:row.word,
-        category:row.category,
-        hints:row.hints,
-    }));
+    _wordData=wordData;
     return _wordData;
 }
 
--- src/WordGame/RoleRevealScreen.js
+++ src/WordGame/RoleRevealScreen.js
@@ -2,26 +2,19 @@ import {View,Text,StyleSheet,ImageBackground,TouchableOpacity,Animated,Image,Act
 import {useState,useEffect,useRef} from 'react';
 import { useGame } from '../GameContext';
 import {getRandomWord,getRandomHint,getCategoryLabel} from './GamePlayFunctions';
-import RNShake from 'react-native-shake';
+import {Accelerometer} from 'expo-haptics';
 
 export default function RoleRevealScreen({navigation}){
     
     return(
+        <ImageBackground source={require('../../assets/Images/HomeImage.png')} style={styles.background} resizeMode="cover">
+
+        </ImageBackground>
     );
 }
 
 const styles=StyleSheet.create({
-    container:{
-        // flex:1,
-        // justifyContent:'center',
-        // alignItems:'center',
-        // backgroundColor:'white',
-    },
-    title:{
-        // fontSize:24,
-        // fontWeight:'bold',
-        // color:'black',
-        // justifyContent:'center',
-        // alignItems:'center',
+    background:{
+        flex:1,
     },
 });
